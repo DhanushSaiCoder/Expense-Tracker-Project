a69a7e507fb6e3f3e10f9c84713e9b1a311b3cbe
DhanushSaiCoder <dhanushsai1467@gmail.com>
2024-06-24 01:13:12 +0530

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 let expenseBtn = document.getElementById('expenseBtn');
 let reportBtn = document.getElementById('reportBtn');
 let entryDiv = document.getElementById('entryDiv');
-
+let statsDiv = document.getElementById('statsDiv');
 function toReport(){
   expenseBtn.classList.remove('active');
   reportBtn.classList.add('active');
   entryDiv.style.display='none';
+  statsDiv.style.display='none';
 }
 function toEntry() {
   expenseBtn.classList.add('active');
   reportBtn.classList.remove('active');
   entryDiv.style.display='flex';
+  statsDiv.style.display='block';
 }
